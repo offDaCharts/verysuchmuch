aee4bfbca379b280ae489974fdf3993483e27fbc
Sam Verhasselt <sam.verhasselt@datapop.com>
2014-02-08 14:39:42 -0800

    Added DogePay.com api creds.

--- verysuchmuch.py
+++ verysuchmuch.py
@@ -8,7 +8,9 @@ from functools import wraps
 app = Flask(__name__)
 app.config.from_object(__name__)
 app.secret_key = 'this key is so secret'
-      
+
+DOGEPAY_API_KEY = '1b94bu21lq55xg1xct9z4trgnzc'
+DOGEPAY_BASE_URL= 'https://www.dogeapi.com/wow/?api_key={0}&a='.format(DOGEPAY_API_KEY)
       
 @app.route('/')
 def show_home():
