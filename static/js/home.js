cc319c3bcd7e0d180fe505051383e06de00cba4e
Quin <rqthames@yahoo.com>
2014-02-08 19:15:03 -0800

    Javascript validation with error alert message

--- static/js/home.js
+++ static/js/home.js
@@ -21,7 +21,27 @@ $(function() {
                     });
                 }
             );
+        },
+        validate = function() {
+            if (isNaN(+$("#dogeAmount").val()) || isNaN(+$("#dollarAmount").val())) {
+                $("#errorMessage").show();                
+                $("#errorMessage").text("Must enter a number");
+                return 0;
+            } else if(+$("#dogeAmount").val() < 5) {
+                $("#errorMessage").show();                
+                $("#errorMessage").text("Must be 5 or more Doge");
+                return 0; 
+            } else if (!$("#dogeWallet").val().match(/^D[A-Z0-9][A-Za-z0-9]{30}$/)) {
+                $("#errorMessage").show();                
+                $("#errorMessage").text("Invalid Doge Address");                
+                return 0;
+            } else {
+                $("#errorMessage").hide();                
+                return 1;
+            }
         };
+
+    $("#errorMessage").hide();                
 
     $("#dogeAmount").keyup(function() {
         if (isNaN(+$(this).val())) {
@@ -29,6 +49,7 @@ $(function() {
         } else {
             $("#dollarAmount").val(+$(this).val()*dogeToDollarRate);
         }
+        validate();
     });
 
     $("#dollarAmount").keyup(function() {
@@ -37,15 +58,30 @@ $(function() {
         } else {
             $("#dogeAmount").val(Math.floor(+$(this).val()*dollarToDogeRate));
         }
+        validate();
+    });
+
+    $("#dogeWallet").keyup(function() {
+        validate();
     });
 
     $("#purchaseButton").click(function(e) {
         e.preventDefault();
+        if (isNaN(+$("#dollarAmount").val())) {
+            $("#dogeAmount").val("");
+        } else {
+            $("#dogeAmount").val(Math.floor(+$("#dollarAmount").val()*dollarToDogeRate));
+        }
+
         var flooredDoge = Math.floor(+$("#dogeAmount").val()),
             dollarRoundedUpCent = Math.ceil(+$("#dollarAmount").val()*100)/100;
+
         $("#dogeAmount").val(flooredDoge);
         $("#dollarAmount").val(dollarRoundedUpCent);
-        purchase(dollarRoundedUpCent);
+
+        if(validate()) {
+            purchase(dollarRoundedUpCent);
+        }
     });
 
 
