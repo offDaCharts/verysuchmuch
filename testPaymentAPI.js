b41cf02f5c4641fba8e072261d0aae88a08ebb7d
Sam Verhasselt <sam.verhasselt@datapop.com>
2014-02-08 16:18:01 -0800

    Made template adjustments.

--- testPaymentAPI.js
+++ testPaymentAPI.js
@@ -2,12 +2,12 @@ $(function(){
     //Success handler
     var successHandler = function(purchaseAction){
         console.log("Purchase completed successfully.");
-    }
+    };
 
     //Failure handler
     var failureHandler = function(purchaseActionError){
         console.log("Purchase did not complete.");
-    }
+    };
 
     function purchase(){
       google.payments.inapp.buy({
